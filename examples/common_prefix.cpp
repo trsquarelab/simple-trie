b0dd3bf0034c0c1d745a28380df0a07ce4117534
valappil <valappil@localhost>
2012-06-07 15:35:33 +0530

    left iterator issues with one element fixed

---
+++ examples/common_prefix.cpp
@@ -0,0 +1,20 @@
+#include <trie.h>
+#include <string>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char ** argv)
+{
+    rtv::Trie<char, std::string> dictionary('\0');
+
+    dictionary.insert("karma", "Destiny or fate, following as effect from cause");
+    rtv::Trie<char, std::string>::Iterator iter = dictionary.startsWith("kar");
+
+    std::cout << "Keys with prefix kar are " << std::endl;
+
+    for (; iter != dictionary.end(); ++iter) {
+        std::cout << iter->first << " : " << iter->second->c_str() << std::endl;
+    }
+
+    return 0;
+}
---
+++ examples/retrieval.cpp
@@ -0,0 +1,33 @@
+#include <trie.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+
+    rtv::Trie<char, std::string> dictionary('$');
+
+    dictionary.insert("karma$", "Destiny or fate, following as effect from cause");
+
+    if (dictionary.hasKey("karma$")) {
+        std::cout << "key karma found" << std::endl;
+    }
+    std::string * result = dictionary.get("karma$");
+    if (result) {
+        std::cout << "Found karma using get" << std::endl;
+        std::cout << "karma:" << result->c_str() << std::endl;
+    }
+
+    rtv::Trie<char, std::string>::Iterator iter = dictionary.find("karma$");
+    if (iter != dictionary.end()) {
+        std::cout << "Found karma using find" << std::endl;
+        int i = 0;
+        for (; iter->first[i] != dictionary.endSymbol(); ++i) {
+        }
+        std::string keyWithNullTermination(iter->first, i);
+        std::cout << keyWithNullTermination.c_str() << ":" << iter->second->c_str() << std::endl;
+    }
+
+
+    return 0;
+}
