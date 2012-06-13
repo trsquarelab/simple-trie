bbd09d0a20c5a6adf18063e3263564f9e222e6bb
ranjith <ranjith@localhost>
2012-06-13 20:29:42 +0530

    license header added

--- examples/common.cpp
+++ examples/common.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2012, Ranjith TV
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause ("BSD New" or "BSD Simplified") license.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ */
 
 #ifndef TRIE_EXAMPLES_H
 #define TRIE_EXAMPLES_H
