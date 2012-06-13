c9eac86f01403aeb333e29356e0059ee4dfd7b6c
ranjith <ranjith@localhost>
2012-06-13 20:29:42 +0530

    license header added

--- examples/common_prefix.cpp
+++ examples/common_prefix.cpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2012, Ranjith TV
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause ("BSD New" or "BSD Simplified") license.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
 #include <trie.h>
 #include <string>
 #include <iostream>
